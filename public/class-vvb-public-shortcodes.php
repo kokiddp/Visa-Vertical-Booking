7d8389f8a808f8d73ec66eb0482019167d42beaa
Gabriele <info@elk-lab.com>
2018-11-23 11:27:43 +0100

    try

--- public/class-vvb-public-shortcodes.php
+++ public/class-vvb-public-shortcodes.php
@@ -72,7 +72,7 @@ class Vvb_Public_Shortcodes {
 
 				<input type="date" ng-model="form.departDate">
 
-				<select ng-model="form.rooms">
+				<select ng-model="form.rooms" ng-init="form.rooms = 1">
 					<option ng-repeat="n in [].constructor(5) track by $index+1">{{$index+1}}</option>
 				</select>
 				<span>{{form.rooms}}</span>
