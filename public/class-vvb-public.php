800f87f183c3dc64d27fb03026c7513d96b950e0
Gabriele <info@elk-lab.com>
2018-11-23 16:17:23 +0100

    typo

--- public/class-vvb-public.php
+++ public/class-vvb-public.php
@@ -120,7 +120,7 @@ class Vvb_Public {
 		 */
 
 		if ( $this->environment == 'production' ) {
-			wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/vvb-public-min.js', array( 'jquery' ), $this->version, false );
+			wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/vvb-public.min.js', array( 'jquery' ), $this->version, false );
 		}
 		else {
 			wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/vvb-public.js', array( 'jquery' ), time(), false );
