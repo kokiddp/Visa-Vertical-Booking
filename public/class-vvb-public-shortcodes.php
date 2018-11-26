4c2da0e1bd911dec55d349e95284d5eb85f26626
Gabriele <info@elk-lab.com>
2018-11-26 12:09:58 +0100

    add class names

--- public/class-vvb-public-shortcodes.php
+++ public/class-vvb-public-shortcodes.php
@@ -64,48 +64,47 @@ class Vvb_Public_Shortcodes {
 		ob_start();
 		?>
 
-		<div id="angular-app" ng-app="vvb" ng-controller="vvbController" ng-cloak ng-strict-di>
+		<div id="vvb" ng-app="vvb" ng-controller="vvbController" ng-cloak ng-strict-di>
 
-			<form name="vvbForm" ng-init="
-				internal.url='<?= $this->options['url'] ?>';
-				submit.id_albergo=<?= $this->options['id_albergo'] ?>;
-				submit.id_stile=<?= $this->options['id_stile'] ?>;
-				submit.dc=<?= $this->options['dc'] ?>;
-				internal.minNights=<?= $this->options['min_nights'] ?>;
-				internal.maxRooms=<?= $this->options['max_rooms'] ?>;
-				internal.maxPeople=<?= $this->options['max_people'] ?>;
-				internal.defaultAdults=<?= $this->options['default_adults'] ?>;
-				internal.minAdultsFirstRoom=<?= $this->options['min_adults_first_room'] ?>;
-				internal.minAdultsOtherRooms=<?= $this->options['min_adults_other_rooms'] ?>;
-				internal.maxAgeChildren=<?= $this->options['max_age_children'] ?>;
-			" novalidate>
+			<form name="vvbForm" ng-init="internal.url='<?= $this->options['url'] ?>';submit.id_albergo=<?= $this->options['id_albergo'] ?>;submit.id_stile=<?= $this->options['id_stile'] ?>;submit.dc=<?= $this->options['dc'] ?>;internal.minNights=<?= $this->options['min_nights'] ?>;internal.maxRooms=<?= $this->options['max_rooms'] ?>;internal.maxPeople=<?= $this->options['max_people'] ?>;internal.defaultAdults=<?= $this->options['default_adults'] ?>;internal.minAdultsFirstRoom=<?= $this->options['min_adults_first_room'] ?>;internal.minAdultsOtherRooms=<?= $this->options['min_adults_other_rooms'] ?>;internal.maxAgeChildren=<?= $this->options['max_age_children'] ?>;" novalidate>
 
-				<label><?= __( 'Arrival date', 'vvb' ) ?></label>
-				<input name="arrivalDate" type="date" ng-model="form.arrivalDate" ng-min="{{internal.minArrivalDate}}" min="{{internal.minArrivalDate | date:'yyyy-MM-dd'}}">
-				<label ng-if="vvbForm.arrivalDate.$invalid"><?= __( 'Invalid date!', 'vvb' ) ?></label>
-
-				<label><?= __( 'Departure date', 'vvb' ) ?></label>
-				<input name="departDate" type="date" ng-model="form.departDate" ng-min="{{internal.minDepartDate}}" min="{{internal.minDepartDate | date:'yyyy-MM-dd'}}">
-				<label ng-if="vvbForm.departDate.$invalid"><?= __( 'Invalid date!', 'vvb' ) ?></label>
-
-				<span><?= __( 'Rooms: ', 'vvb' ) ?>{{form.rooms.length}}</span>
-				<input type="button" ng-click="addRoom()" ng-disabled="form.rooms.length >= internal.maxRooms" value="<?= __( '+', 'vvb' ) ?>" />
-				<input type="button" ng-click="removeRoom()" ng-disabled="form.rooms.length == 1" value="<?= __( '-', 'vvb' ) ?>" />
-
-				<div ng-repeat="x in form.rooms">
-					<label><?= __( 'Room ', 'vvb' ) ?>{{x.id}}</label>
-					<label><?= __( 'Adults', 'vvb' ) ?></label>
-					<select ng-model="x.adulti" ng-options="n for n in [] | range:x.minAdulti:(x.maxAdulti - x.bambini + 1)"></select>
-					<label><?= __( 'Children', 'vvb' ) ?></label>
-					<select ng-model="x.bambini" ng-options="n for n in [] | range:x.minBambini:(x.maxBambini - x.adulti + 1)"></select>
-					<div ng-repeat="y in [] | range:1:(x.bambini + 1)">
-						<label><?= __( 'Child age ', 'vvb' ) ?>{{y}}</label>
-						<select ng-model="form.ages[x.id][y]" ng-options="n for n in [] | range:0:(internal.maxAgeChildren + 1)" ng-required="true"></select>
+				<div class="vvb_dates">
+					<div class="vvb_date vvb_date_arrival">
+						<label><?= __( 'Arrival date', 'vvb' ) ?></label>
+						<input name="arrivalDate" type="date" ng-model="form.arrivalDate" ng-min="{{internal.minArrivalDate}}" min="{{internal.minArrivalDate | date:'yyyy-MM-dd'}}">
+						<label ng-if="vvbForm.arrivalDate.$invalid"><?= __( 'Invalid date!', 'vvb' ) ?></label>
+					</div>
+					<div class="vvb_date vvb_date_depart">
+						<label><?= __( 'Departure date', 'vvb' ) ?></label>
+						<input name="departDate" type="date" ng-model="form.departDate" ng-min="{{internal.minDepartDate}}" min="{{internal.minDepartDate | date:'yyyy-MM-dd'}}">
+						<label ng-if="vvbForm.departDate.$invalid"><?= __( 'Invalid date!', 'vvb' ) ?></label>
 					</div>
 				</div>
 
-				<input type="submit" ng-click="submitForm()" ng-disabled="vvbForm.$invalid" value="<?= __( 'Submit', 'vvb' ) ?>" />
-				<label ng-if="vvbForm.$invalid"><?= __( 'There are one or more errors in your request. Please correct them before submitting.', 'vvb' ) ?></label>
+				<div class="vvb_rooms_controls">
+					<span><?= __( 'Rooms: ', 'vvb' ) ?>{{form.rooms.length}}</span>
+					<input type="button" ng-click="addRoom()" ng-disabled="form.rooms.length >= internal.maxRooms" value="<?= __( '+', 'vvb' ) ?>" />
+					<input type="button" ng-click="removeRoom()" ng-disabled="form.rooms.length == 1" value="<?= __( '-', 'vvb' ) ?>" />
+				</div>
+
+				<div class="vvb_rooms">
+					<div ng-repeat="x in form.rooms" class="vvb_room">
+						<label><?= __( 'Room ', 'vvb' ) ?>{{x.id}}</label>
+						<label><?= __( 'Adults', 'vvb' ) ?></label>
+						<select ng-model="x.adulti" ng-options="n for n in [] | range:x.minAdulti:(x.maxAdulti - x.bambini + 1)"></select>
+						<label><?= __( 'Children', 'vvb' ) ?></label>
+						<select ng-model="x.bambini" ng-options="n for n in [] | range:x.minBambini:(x.maxBambini - x.adulti + 1)"></select>
+						<div ng-repeat="y in [] | range:1:(x.bambini + 1)">
+							<label><?= __( 'Child age ', 'vvb' ) ?>{{y}}</label>
+							<select ng-model="form.ages[x.id][y]" ng-options="n for n in [] | range:0:(internal.maxAgeChildren + 1)" ng-required="true"></select>
+						</div>
+					</div>
+				</div>
+
+				<div class="vvb_submit">
+					<input type="submit" ng-click="submitForm()" ng-disabled="vvbForm.$invalid" value="<?= __( 'Submit', 'vvb' ) ?>" />
+					<label ng-if="vvbForm.$invalid"><?= __( 'There are one or more errors in your request. Please correct them before submitting.', 'vvb' ) ?></label>
+				</div>
 			</form>
 
 		</div>		
